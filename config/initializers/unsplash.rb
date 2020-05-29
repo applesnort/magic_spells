5862d3db5718fedb10d4a3ab2ca155958b1f3126
applesnort <jmangin@gmail.com>
2020-05-30 07:05:57 +0800

    added app name to utm

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -2,7 +2,7 @@ Unsplash.configure do |config|
   config.application_access_key = Rails.application.credentials.UNSPLASH_ACCESS_KEY
   config.application_secret = Rails.application.credentials.UNSPLASH_SECRET_KEY
   config.application_redirect_uri = "https://your-application.com/oauth/callback"
-  config.utm_source = "alices_terrific_client_app"
+  config.utm_source = "Magician's Journal"
 
   # optional:
   # config.logger = MyCustomLogger.new
