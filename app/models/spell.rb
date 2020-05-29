aa1433c43e2f2de4e2828585109af89adf89710a
applesnort <jmangin@gmail.com>
2020-05-29 14:30:26 +0800

    spell model validation part 1

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,2 +1,7 @@
 class Spell < ApplicationRecord
+  # has_many :doses, dependent: :destroy
+  # has_many :ingredients, through: :doses
+  validates :name, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :level, presence: true
 end
