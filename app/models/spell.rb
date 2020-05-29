d08d71c59ca4612f6aceb6994c570d76781be767
applesnort <jmangin@gmail.com>
2020-05-30 07:39:34 +0800

    trying to get the unsplash config error solved

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -6,5 +6,12 @@ class Spell < ApplicationRecord
   
   validates :name, presence: true, uniqueness: true
   validates :kind, presence: true
-  validates :level, presence: true  
+  validates :level, presence: true
+
+  def categories
+    array = []
+    self.all.each { |item| array << item.kind }
+    return array.uniq
+  end
+  
 end
--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,7 +1,7 @@
 Unsplash.configure do |config|
   config.application_access_key = Rails.application.credentials.UNSPLASH_ACCESS_KEY
   config.application_secret = Rails.application.credentials.UNSPLASH_SECRET_KEY
-  config.application_redirect_uri = "https://your-application.com/oauth/callback"
+  config.application_redirect_uri = "https://lit-springs-15102.herokuapp.com/oauth/callback"
   config.utm_source = "Magician's Journal"
 
   # optional:
