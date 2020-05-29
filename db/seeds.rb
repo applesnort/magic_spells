859c8c9f9459c632ed5ef9ddbb07538ab53f625d
applesnort <jmangin@gmail.com>
2020-05-29 14:55:47 +0800

    added Faker and hopeful seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+if Rails.env.development?
+  Dose.destroy_all
+  Spell.destroy_all
+  Ingredient.destroy_all
+end
+
+SPELL_TYPE = []
+
+3.times do |item|
+  c = Spell.create!(
+    name: "#{Faker::GreekPhilosophers.name} #{Faker::Construction.subcontract_category} #{Faker::Hipster.words(number: 2, supplemental: true)}",
+    type: Faker::Company.bs,
+    level: rand(1..1300)
+    )
+  item['ingredients'].each do |ingredient|
+  i = Ingredient.find_or_create_by(
+    name: "#{Faker::Dessert.flavor} #{Faker::Hipster.words(number: 2, supplemental: true, spaces_allowed: true)}")
+  d = Dose.create(spell: c, ingredient: i, description: "#{Faker::Hipster.sentence(word_count: 2, supplemental: false, random_words_to_add: 2)}")
+  end
+end
