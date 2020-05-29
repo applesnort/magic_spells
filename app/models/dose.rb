bbcad7f0e5b52d270c02c0b3e7d03ce563e9392f
applesnort <jmangin@gmail.com>
2020-05-29 14:39:21 +0800

    validations and associations please work

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :spell
   belongs_to :ingredient
+  validates :instructions, presence: true
+  validates :ingredient_id, presence: true, uniqueness: { scope: :spell_id } 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_061550) do
+ActiveRecord::Schema.define(version: 2020_05_29_063605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doses", force: :cascade do |t|
+    t.string "instructions"
+    t.bigint "spell_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+    t.index ["spell_id"], name: "index_doses_on_spell_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "potency"
+    t.string "rarity"
+    t.string "effect"
+    t.string "benefit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "spells", force: :cascade do |t|
     t.string "name"
@@ -23,4 +43,6 @@ ActiveRecord::Schema.define(version: 2020_05_29_061550) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "doses", "ingredients"
+  add_foreign_key "doses", "spells"
 end
