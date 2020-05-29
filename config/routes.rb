aa73a6ba3048759f34a7e3e964086f3091ea0057
applesnort <jmangin@gmail.com>
2020-05-29 14:18:34 +0800

    spells controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'spells/index'
+  get 'spells/new'
+  get 'spells/create'
+  get 'spells/edit'
+  get 'spells/update'
+  get 'spells/show'
+  get 'spells/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/spells_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class SpellsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get spells_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get spells_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get spells_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get spells_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get spells_update_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get spells_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get spells_destroy_url
+    assert_response :success
+  end
+
+end
