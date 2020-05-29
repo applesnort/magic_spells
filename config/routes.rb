6d2defb199d06cdcd7cfa344d6c9981b08869989
applesnort <jmangin@gmail.com>
2020-05-29 14:25:54 +0800

    routes and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'spells/index'
-  get 'spells/new'
-  get 'spells/create'
-  get 'spells/edit'
-  get 'spells/update'
-  get 'spells/show'
-  get 'spells/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "spells#index", as: :home
+  resources :spells
 end
