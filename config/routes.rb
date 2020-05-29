ad8b3e8eba7ee7bc99c9bc4aadf1e256f3360f0d
applesnort <jmangin@gmail.com>
2020-05-29 14:35:20 +0800

    ingredients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/edit'
+  get 'ingredients/update'
+  get 'ingredients/show'
+  get 'ingredients/destroy'
   root "spells#index", as: :home
   resources :spells
 end
