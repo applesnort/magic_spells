a5d7df2eeda7fb6661a0b4b7cfe5eda6cd923b65
applesnort <jmangin@gmail.com>
2020-05-29 14:40:31 +0800

    nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'ingredients/index'
-  get 'ingredients/new'
-  get 'ingredients/create'
-  get 'ingredients/edit'
-  get 'ingredients/update'
-  get 'ingredients/show'
-  get 'ingredients/destroy'
   root "spells#index", as: :home
-  resources :spells
+  resources :spells do
+    resources :ingredients
+  end
 end
